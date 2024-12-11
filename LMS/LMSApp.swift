b33b47cc7fb338419a8e43699f5038d28abbcd52
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2024-12-11 13:16:51 +0530

    Initial Commit

---
+++ LMS/LMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMSApp.swift
+//  LMS
+//
+//  Created by Sudhanshu Singh Rajput on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LMS/Splash Screen.swift
@@ -0,0 +1,25 @@
+//
+//  Splash Screen.swift
+//  LMS
+//
+//  Created by Sudhanshu Singh Rajput on 11/12/24.
+//
+
+import SwiftUI
+
+struct Splash_Screen: View {
+    var body: some View {
+        VStack{
+            VStack{
+                Image("hare.fill")
+                    .resizable()
+                    .scaledToFit()
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    Splash_Screen()
+}
