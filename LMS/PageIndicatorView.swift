3216d8e97238c6be9c3aa7c75afbfa6c140ec6c3
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2024-12-11 23:15:04 +0530

    Create Splash_Screen , Onboarding_Screen and Some Part of Sign In Screen.

---
+++ LMS/PageIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  PageIndicatorView.swift
+//  LMS
+//
+//  Created by Sudhanshu Singh Rajput on 11/12/24.
+//
+
+import SwiftUI
+
+struct PageIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageIndicatorView()
+}
---
+++ LMS/SignInAndSignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInAndSignUpView.swift
+//  LMS
+//
+//  Created by Sudhanshu Singh Rajput on 11/12/24.
+//
+
+import SwiftUI
+
+struct SignInAndSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInAndSignUpView()
+}
